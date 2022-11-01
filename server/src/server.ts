446773cc2b2ae45297524b8679e061259fde357b
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2022-11-01 10:07:28 -0300

    feat: fastiofy cors add

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,28 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true, //gerar logs
     })
 
-    fastify.get('/pools/count', () => {
-        return { count: 5616551 }
+    // CONTAGEM DE BOLÕES
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        // const pools = await prisma.pool.findMany({
+        //     where: {
+        //         code: {
+        //             startsWith: 'B'
+        //         }
+        //     }
+        // })
+
+        return { count }
     })
 
     await fastify.listen({ port: 3333 })
