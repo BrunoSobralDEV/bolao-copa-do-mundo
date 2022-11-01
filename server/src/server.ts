119f07383e400ce06f03e3437765d2add01a08c7
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2022-11-01 10:09:38 -0300

    feat: setup back-end finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -8,6 +9,10 @@ const prisma = new PrismaClient({
 async function bootstrap() {
     const fastify = Fastify({
         logger: true, //gerar logs
+    })
+
+    await fastify.register(cors, {
+        origin: true, // Qualquer aplicação
     })
 
     // CONTAGEM DE BOLÕES
@@ -25,7 +30,7 @@ async function bootstrap() {
         return { count }
     })
 
-    await fastify.listen({ port: 3333 })
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
