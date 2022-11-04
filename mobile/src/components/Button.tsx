d097460c80362735b12cf8a4685dd748c9d022d0
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2022-11-04 11:52:59 -0300

    feat: button signin

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,13 +1,33 @@
 import { Button as ButtonNativeBase, Text, IButtonProps } from 'native-base';
 
 interface ButtonProps extends IButtonProps {
-  title: string
+  title: string;
+  type?: 'PRIMARY' | 'SECONDARY'
 }
 
-export function Button({title}: ButtonProps) {
+export function Button({title, type = 'PRIMARY', ...rest}: ButtonProps) {
   return (
-    <ButtonNativeBase>
-      <Text>
+    <ButtonNativeBase 
+      w='full'
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+      }}
+      _loading={{
+        _spinner: { color: 'black'}
+      }}
+
+      {...rest}
+    >
+      <Text
+        fontSize='sm'
+        fontFamily='heading'
+        color={type === 'SECONDARY' ? 'white' : 'black'}
+      >
         {title}
       </Text>
     </ButtonNativeBase>
