f6b1c8ded7d41dc526010611412de952dee3c184
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2022-11-09 11:52:30 -0300

    feat: get list of pools by owner

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,19 +1,52 @@
 import { useState } from 'react'
-import { Alert } from 'react-native'
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, VStack, useToast } from "native-base";
 
 import Logo from '../assets/logo.svg';
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
+import { api } from '../services/api';
 
 export function New(){
   const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
 
   async function handlePoolCreate() {
-    if(!title) {
-      Alert.alert('Opa!', 'Informe o título')
+    if(!title.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+
+    try {
+      setIsLoading(true)
+
+      await api.post('/pools', { title })
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+      setTitle('')
+
+    } catch (error) {
+     console.log(error) 
+     
+     toast.show({
+      title: 'Não foi possível criar o bolão.',
+      placement: 'top',
+      bgColor: 'red.500'
+      })
+
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -39,6 +72,7 @@ export function New(){
         <Button
           title='CRIAR MEU BOLÃO'
           onPress={handlePoolCreate}
+          isLoading={isLoading}
         />
 
         <Text color='gray.200' fontSize='sm' textAlign='center' px={10} mt={4}>
