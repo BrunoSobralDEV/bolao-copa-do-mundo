d958ed2444c34822c2f76c8feaddebfbd52b7e5c
Bruno Sobral <brunosobralss@hotmail.com>
2022-12-07 20:18:18 -0300

    finish app

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -28,7 +28,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   const [isUserLoading, setIsUserLoading] = useState(false);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: '453716288201-1fnc9ee5j5gcqlse5et2nvop8ro0dbkk.apps.googleusercontent.com',
+    clientId: process.env.CLIENT_ID,
     redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
     scopes: ['profile', 'email']
   })
